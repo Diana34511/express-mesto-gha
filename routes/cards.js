468003f8f37cce65b8566982857809c28b9f656a
Diana <diana34511@gmail.com>
2022-07-15 13:31:01 +0200

    add possibility like and dislake cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,21 @@
 const router = require("express").Router();
 const CardsModel = require("../models/cards");
-const { createCard, getCards, deleteCard } = require("../controllers/cards");
+const {
+  createCard,
+  getCards,
+  deleteCard,
+  likeCard,
+  dislikeCard,
+} = require("../controllers/cards");
 
 router.get("/", getCards);
 
 router.post("/", createCard);
 
 router.delete("/:cardId", deleteCard);
+
+router.put("/:cardId/likes", likeCard);
+
+router.delete("/:cardId/likes", dislikeCard);
 
 module.exports = router;
